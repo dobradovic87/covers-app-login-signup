4e35722ce5e98ac61dc3527b0ab5832ac25d4d7e
Dragan Obradovic <48182445+dobradovic87@users.noreply.github.com>
2021-10-12 13:41:22 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  firebase: {
-    projectId: 'covers-search-94ab7',
-    appId: '1:466808810342:web:c848363fec07b67d6276a7',
-    databaseURL: 'https://covers-search-94ab7-default-rtdb.firebaseio.com',
-    storageBucket: 'covers-search-94ab7.appspot.com',
-    apiKey: 'AIzaSyDYQwmQQqbqLsbmMuwRwVdfXF46QsuDywA',
-    authDomain: 'covers-search-94ab7.firebaseapp.com',
-    messagingSenderId: '466808810342',
-    measurementId: 'G-WTBY2QN87L',
+  // add your firebaseConfig here
   },
   production: false,
   apiKey: '8960814a99312e10f3209b628cd9784d',
@@ -21,7 +13,7 @@ export const environment = {
     'https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&format=json&limit=30&artist=',
   albumInfoUrl:
     'https://ws.audioscrobbler.com/2.0/?method=album.getinfo&format=json',
-  youTubeApiKey: 'AIzaSyARA9__SPutUAS5xWYVw68SX80YIgd8l_0',
+  //youTubeApiKey: 'your YouTube API key',
 };
 
 /*
